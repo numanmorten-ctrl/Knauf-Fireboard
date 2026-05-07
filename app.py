1283655e161b02590238163cd8ad5d09b5a7604d
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-07 21:12:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -441,9 +441,25 @@ if custom_profile:
 
     else:
 
-        st.info(
-            "Ap/V = indvendig omkreds divideret "
-            "med stålets tværsnitsareal × 1000"
+        st.markdown("""
+        ### Beregning af profilforhold
+
+        Beregning udføres iht. **EN 13381-4** og  
+        **DS/EN 1993-1-2 (Eurocode 3)**.
+        """)
+
+        st.latex(
+            r'''
+            \frac{A_p}{V}
+            =
+            \frac{\text{Indvendig omkreds [mm]}}
+            {\text{Tværsnitsareal [mm^2]}}
+            \times 1000
+            '''
+        )
+
+        st.caption(
+            "Resultat angives i m²/m³"
         )
 
         perimeter = st.number_input(
