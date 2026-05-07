3f6e07cc7b79eff4de07bf1ffbe83fb20d2bfdb3
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-07 21:26:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -441,29 +441,37 @@ if custom_profile:
 
     else:
 
-        st.markdown("""
-        ### Beregning af profilforhold
-
-        Beregning udføres iht. **EN 13381-4** og  
-        **DS/EN 1993-1-2 (Eurocode 3)**.
-        """)
-
-        st.latex(
-            r'''
-            \frac{A_p}{V}
-            =
-            \frac{
-                \mathrm{Indvendig\ omkreds\ [mm]}
-            }{
-                \mathrm{Tværsnitsareal\ [mm^2]}
-             }
-            \times 1000
-            '''
+        st.markdown(
+            "### Beregning af profilforhold"
         )
 
-        st.caption(
-            "Resultat angives i m²/m³"
-        )
+        col1, col2 = st.columns([1, 1])
+
+        with col1:
+
+            st.markdown("""
+            Beregning udføres iht. **EN 13381-4**  
+            og **DS/EN 1993-1-2 (Eurocode 3)**.
+            """)
+
+            st.caption(
+                "Resultat angives i m²/m³"
+            )
+
+        with col2:
+
+            st.latex(
+                r'''
+                \frac{A_p}{V}
+                =
+                \frac{
+                    \mathrm{Indvendig\ omkreds\ [mm]}
+                }{
+                    \mathrm{Tværsnitsareal\ [mm^2]}
+                }
+                \times 1000
+                '''
+            )
 
         perimeter = st.number_input(
             "Indvendig omkreds (mm)",
