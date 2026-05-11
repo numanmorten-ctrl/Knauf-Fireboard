bc3ea53b5f3e451d3ca1943a72cf4db5cdf9cc9c
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-11 23:00:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1066,7 +1066,7 @@ def card(
         padding:10px;
         width:calc(100% - 2px);
         text-align:center;
-        height:135px;
+        height:160px;
         width:100%;
         box-sizing:border-box;
         display:flex;
@@ -1146,7 +1146,7 @@ def disabled_card(
         padding:10px;
         width:calc(100% - 2px);
         text-align:center;
-        height:135px;
+        height:160px;
         width:100%;
         box-sizing:border-box;
         display:flex;
