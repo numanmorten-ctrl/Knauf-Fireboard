79a4295ac0416332ba6522337e84bf52f2b1683e
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-11 13:57:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -743,12 +743,6 @@ fire_tables = {
 
 col1, col2, col3 = st.columns([6, 2, 2])
 
-with col1:
-
-    st.title(
-        "Brandbeskyttelse af stålkonstruktioner"
-    )
-
 # ---------------------------------------------------
 # NY BEREGNING
 # ---------------------------------------------------
