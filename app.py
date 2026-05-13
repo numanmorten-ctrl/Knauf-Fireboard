f30360e9be73b97ec1905f8c113eecb7c6733dea
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-13 14:12:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2056,7 +2056,7 @@ if current_step == 0:
         st.markdown("""
         <div style="
             font-size:15px;
-            font-weight:400;
+            font-weight:500;
             color:#3e4650;
             margin-bottom:0.3rem;
         ">
