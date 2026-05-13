6294af534af31be3be66f6f7bec21e17d54833eb
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-13 12:08:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1872,6 +1872,10 @@ temperature = st.session_state.get(
     450
 )
 
+selected_profile = st.session_state.get(
+    "selected_profile"
+)
+
 apv = None
 thickness = None
 
@@ -2466,9 +2470,6 @@ if current_step == 2:
 # ---------------------------------------------------
 # FIND AP/V + TYKKELSE
 # ---------------------------------------------------
-selected_profile = st.session_state.get(
-    "selected_profile"
-)
 
 apv = None
 thickness = None
