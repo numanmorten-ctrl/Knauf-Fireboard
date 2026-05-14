1c484ebaba6896d6871e504a13b3807178b73a25
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-14 21:14:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -727,7 +727,7 @@ div[data-testid="stSelectbox"] {
 
 div[data-baseweb="select"] > div {
 
-    padding-left: 220x !important;
+    padding-left: 22Px !important;
 
     min-height: 44px !important;
 
