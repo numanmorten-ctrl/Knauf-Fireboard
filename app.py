24007fdd48e9a90b422bcf164737d7c8ee5ac9a5
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-15 00:18:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -777,7 +777,7 @@ div[data-testid="stButton"] button:disabled {
 
     cursor: default !important;
 
-    margin-right: -6px !important;
+    margin-right: -10px !important;
 }
 
 /* ---------------------------------------------------
