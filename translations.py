43151c595243420bd39ada2f104aa84bc1affa3c
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-14 08:48:27 +0200

    Create translations.py

---
+++ translations.py
@@ -0,0 +1,38 @@
+translations = {
+
+    "DA": {
+
+        "title":
+            "Brandbeskyttelse af stålkonstruktioner",
+
+        "next":
+            "Næste →",
+
+        "previous":
+            "← Forrige",
+
+        "new_calculation":
+            "🔄 Ny beregning",
+
+        "new_project":
+            "🗑️ Nyt projekt",
+    },
+
+    "EN": {
+
+        "title":
+            "Fire protection of steel structures",
+
+        "next":
+            "Next →",
+
+        "previous":
+            "← Previous",
+
+        "new_calculation":
+            "🔄 New calculation",
+
+        "new_project":
+            "🗑️ New project",
+    }
+}
