b39e2afe9d25fef091980267c1d501cf83010219
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-20 12:13:41 +0200

    Change CSV separator from ',' to ';'

--- app.py
+++ app.py
@@ -1087,7 +1087,7 @@ apv_df = load_and_clean_csv(
 
 beam_df = pd.read_csv(
     DATA_DIR / "CSV_3.csv",
-    sep=",")
+    sep=";")
 st.write(beam_df.columns.tolist())
 # ---------------------------------------------------
 # CLEAN APV DATA
