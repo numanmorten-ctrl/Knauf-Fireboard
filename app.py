a2921197dd05fb5df617c1398349f1949fedaa44
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-22 11:01:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ from utils.material_helpers import (
     resolve_angle_material,
     resolve_screw_material,
     resolve_staple_material,
+    generate_fastener_materials,
 )
 
 from reportlab.platypus import (
