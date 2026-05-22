ea3457c1155c3ff99eb9ad4c448870c7fb76520c
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-22 22:15:54 +0200

    Update pdf_generator.py

--- utils/pdf_generator.py
+++ utils/pdf_generator.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 from datetime import datetime
 
+from utils.constants import *
+
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
@@ -26,58 +28,6 @@ PROFILE_IMAGE_MAP = {
 
     "Andre profiler": "images/other_profiles.png",
 }
-
-# ---------------------------------------------------
-# PDF COORDINATES
-# ---------------------------------------------------
-
-# PROJECT INFO
-
-PROJECT_X = 197
-PROJECT_Y = 570
-PROJECT_LINE_HEIGHT = 20
-
-# DESCRIPTION
-
-DESCRIPTION_Y = 482
-DESCRIPTION_MAX_CHARS = 72
-
-# CALCULATION
-
-CALC_X = 197
-CALC_Y = 436
-CALC_LINE_HEIGHT = 19
-
-# RESULT
-
-RESULT_X = 287
-RESULT_Y = 238
-
-# PAGE NUMBER
-
-PAGE_X = 292
-PAGE_Y = 20.4
-
-# PROFILE IMAGE
-
-PROFILE_IMAGE_X = 430
-PROFILE_IMAGE_Y = 730
-PROFILE_IMAGE_WIDTH = 110
-PROFILE_IMAGE_HEIGHT = 110
-
-PROFILE_TEXT_X = 485
-PROFILE_CATEGORY_TEXT_Y = 720
-PROFILE_TEXT_Y = 708
-PROFILE_CATEGORY_FONT = 9
-PROFILE_TEXT_FONT = 11
-
-# FONT SIZES
-
-PROJECT_FONT = 9
-DESCRIPTION_FONT = 9
-CALC_FONT = 9
-RESULT_FONT = 14
-PAGE_FONT = 10
 
 def generate_single_pdf(
     calc,
