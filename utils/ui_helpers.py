216fe11fb78e746e01cda918f167357ce9177c97
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-22 21:28:19 +0200

    Update ui_helpers.py

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -1,2 +1,14 @@
+import base64
+
+
+def get_base64_image(image_path):
+
+    with open(image_path, "rb") as img_file:
+
+        return base64.b64encode(
+            img_file.read()
+        ).decode()
+
+
 def placeholder():
     pass
