c6ba03e29f3c710a0a704a78b54ecc9a7f9c84c8
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-22 19:48:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import requests
 from translations import translations
 from utils.data_loader import clean_text, clean_numeric, load_and_clean_csv
 from utils.render_helpers import render_materials_table
+from utils.validation_helpers import validate_temperature
 from utils.pdf_generator import (
     PROFILE_IMAGE_MAP,
 
