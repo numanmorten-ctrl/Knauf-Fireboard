7eeadca54bef3c528534acb42cce94c551083164
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-22 14:20:16 +0200

    Update pdf_generator.py

--- utils/pdf_generator.py
+++ utils/pdf_generator.py
@@ -1,1 +1,8 @@
+from io import BytesIO
+from datetime import datetime
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+
+from pypdf import PdfReader, PdfWriter
