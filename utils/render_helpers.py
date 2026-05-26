23b84edb5dad3e886fae8b6ab6dcb84140861578
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-26 21:29:15 +0200

    Update render_helpers.py

--- utils/render_helpers.py
+++ utils/render_helpers.py
@@ -3,53 +3,11 @@ import streamlit as st
 
 def render_materials_table(materials_df):
     """
-    Render styled materials table.
+    Render materials table.
     """
 
-    styled_df = (
-        materials_df.style
-
-        # HEADER STYLE
-        .set_table_styles([
-            {
-                "selector": "thead th",
-                "props": [
-                    ("background-color", "#f3f4f6"),
-                    ("color", "#374151"),
-                    ("font-weight", "600"),
-                    ("border-top", "none"),
-                    ("border-left", "none"),
-                    ("border-right", "none"),
-                    ("border-bottom", "1px solid #d1d5db"),
-                    ("padding", "12px"),
-                    ("text-align", "left"),
-                ]
-            },
-
-            # BODY CELLS
-            {
-                "selector": "tbody td",
-                "props": [
-                    ("border-left", "none"),
-                    ("border-right", "none"),
-                    ("border-top", "none"),
-                    ("border-bottom", "1px solid #e5e7eb"),
-                    ("padding", "12px"),
-                ]
-            },
-
-            # TABLE
-            {
-                "selector": "table",
-                "props": [
-                    ("border-collapse", "collapse"),
-                    ("width", "100%"),
-                    ("font-size", "14px"),
-                ]
-            }
-        ])
-
-        .hide(axis="index")
+    st.dataframe(
+        materials_df,
+        use_container_width=True,
+        hide_index=True
     )
-
-    st.table(styled_df)
