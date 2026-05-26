70ff8cc2faa115d1b65309f6f39294ee2b7127d1
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-26 13:26:48 +0200

    Update render_helpers.py

--- utils/render_helpers.py
+++ utils/render_helpers.py
@@ -6,9 +6,4 @@ def render_materials_table(materials_df):
     Render materials table.
     """
 
-    st.table(
-        materials_df.style.set_properties(
-            subset=["ART.NR."],
-            **{"text-align": "right"}
-        )
-    )
+    st.table(materials_df)
