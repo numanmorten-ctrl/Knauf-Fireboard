25f940d1bf2df3e4a83afe2dff61ebf920eda0e4
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-27 12:38:39 +0200

    Update material_helpers.py

--- utils/material_helpers.py
+++ utils/material_helpers.py
@@ -352,9 +352,17 @@ def build_material_row(
         materials_by_description
     )
 
-    per_meter = clean_numeric(per_meter) or 0
-    profile_length = clean_numeric(profile_length) or 0
-    waste_percent = clean_numeric(waste_percent) or 0
+    per_meter = clean_numeric(
+        row.get("Mængde", 0)
+    ) or 0
+
+    profile_length = clean_numeric(
+        profile_length
+    ) or 0
+
+    waste_percent = clean_numeric(
+        waste_percent
+    ) or 0
     
     total = (
         per_meter
