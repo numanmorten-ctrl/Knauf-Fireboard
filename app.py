a294ef3cde581392853a98916af97d606ccc2c2f
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-28 19:46:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3317,7 +3317,7 @@ if current_step == 3:
             f"""
             <div style="
                 text-align: right;
-                padding-right: 18rem;
+                padding-right: 19rem;
                 color: #444444;
                 font-size: 1rem;
                 font-weight: 600;
