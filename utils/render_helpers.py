7bdb67dfd62b377eae1e198944b27390174ffecb
numanmorten-ctrl <numan.morten@gmail.com>
2026-05-28 15:08:50 +0200

    Update render_helpers.py

--- utils/render_helpers.py
+++ utils/render_helpers.py
@@ -6,7 +6,10 @@ def render_materials_table(materials_df):
     Render materials table.
     """
 
-    html = materials_df.to_html(index=False)
+    html = materials_df.to_html(
+        index=False,
+        escape=False
+    )
 
     st.markdown(
         html,
