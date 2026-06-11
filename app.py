e34862abf135809fd8fdff0c00465d9544569f0d
numanmorten-ctrl <numan.morten@gmail.com>
2026-06-11 13:45:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -450,7 +450,7 @@ div[data-baseweb="select"] input {
 div[data-testid="stSelectbox"] div[data-baseweb="select"] > div > div:first-child,
 div[data-testid="stSelectbox"] div[data-baseweb="select"] > div > div:first-child > div {
 
-    transform: translateY(1px);
+    transform: translateY(2px);
 }
 
 /* Dropdown popup */
